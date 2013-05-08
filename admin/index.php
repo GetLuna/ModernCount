9786289245247cecdb7c554da7a57755cde45234
Yannick <contact@studio384.be>
2013-05-08 20:08:15 +0200

    Tag ModernCount 2.0-beta

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,7 @@
  * License: http://www.gnu.org/licenses/gpl.html GPL version 3 or higher
  */
 
-$version = "1.6.9";
+$version = "2.0-beta";
 
 if (!file_exists("../config.php")) {
     header("Location: ../installer");

a8932f0cdacaad8c22c6708436d202823fbc4ddf
Yannick <contact@studio384.be>
2013-06-24 14:07:00 +0200

    Password improvements

--- admin/edit.php
+++ admin/edit.php
@@ -146,10 +146,17 @@ mysql_close($con);
 ?>
 </label>
 </div>
+<div id="passwordentry" style="display: none;">  
+<div class="control-group">  
+<label class="control-label" for="password">Password</label>  
+<div class="controls">  
+<input type="password" id="password" name="password" placeholder="Type a password..." required>  
+</div>  
+</div>  
+</div>  
 </div>
 <div class="form-actions">
 <input type="hidden" name="idtoedit" value="<?php echo $idtoedit; ?>" />
-<input type="hidden" id="password" name="password">
 <button type="submit" class="btn btn-primary">Update</button>
 </div>
 </fieldset>
@@ -163,20 +170,8 @@ mysql_close($con);
 <script type="text/javascript">
 $(document).ready(function() {
     $("#passwordprotectstate").click(function() {
-        if ($("#passwordprotectstate").prop("checked") == true) {
-            password = prompt("Enter a password","");
-            passwordconfirm = prompt("Confirm password","");
-            if (password != passwordconfirm) {
-                alert("Passwords do not match");
-                return false   
-            }
-            if (password != "" && password != null) {
-                $("#password").val(password);
-            } else {
-                $("#passwordprotectstate").prop("checked", false);
-            }
-        }
-    });
+	$("#passwordentry").toggle("fast");  
+  });  
     $("input").not("[type=submit]").jqBootstrapValidation(); 
 });
 </script>
